7c569f4c72e8c18e641b13ecf5820ea9ea1f29a8
Salman Tyagi <salmantyagi15@gmail.com>
2024-11-16 17:57:20 +0530

    removed unwanted logs

--- src/features/movies/ExploreMovies.tsx
+++ src/features/movies/ExploreMovies.tsx
@@ -1,9 +1,4 @@
-import { useParams } from 'react-router-dom';
-
 function ExploreMovies(): JSX.Element {
-  const { movies } = useParams<{ movies: string }>();
-  console.log(movies);
-
   return <div>explore movies</div>;
 }
 
