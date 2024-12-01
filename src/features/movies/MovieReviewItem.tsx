7eddd6f2ca411ab0b75d79aacf75dceeef69321f
Salman Tyagi <salmantyagi15@gmail.com>
2024-12-01 14:54:51 +0530

    added styling for movie reviews

--- src/features/movies/MovieReviewItem.tsx
+++ src/features/movies/MovieReviewItem.tsx
@@ -1,9 +1,16 @@
 import { MovieReview } from './services/apiReleases';
+import { Link } from 'react-router-dom';
+
 import { IoShareSocialOutline } from 'react-icons/io5';
+import { FaStar } from 'react-icons/fa';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { FaRegThumbsDown } from 'react-icons/fa';
 
 interface MovieReviewItemProps {
   review: MovieReview;
 }
+
+const LETTER_COUNT = 160;
 
 function MovieReviewItem({
   review: { email, rating, review, createdAt }
@@ -15,19 +22,50 @@ function MovieReviewItem({
           <img src='/images/default-photo.webp' alt={`${email} photo`} />
         </div>
 
-        <div>
-          <p>{email}</p>
-          <p className='text-sm'>
-            Booked on <span className='text-red-600'>bookmyshow</span>
+        <div className='font-medium'>
+          <p>{email.split('@').at(0)}</p>
+          <p className='space-x-2 text-stone-500'>
+            <span className='text-sm'>Booked on</span>
+            <img
+              className='inline-block max-h-5'
+              src='/images/logo.svg'
+              alt='book-my-show-logo'
+            />
           </p>
         </div>
-        <p className='ml-auto'>{rating}/10</p>
+
+        <p className='ml-auto inline-flex items-center gap-1'>
+          <FaStar className='inline-block text-2xl text-rose-400' />
+          <span className='font-medium'>{rating}/10</span>
+        </p>
       </div>
 
-      <p className='mb-4'>{review}</p>
+      <p className='mb-3 min-h-24 font-medium text-stone-500'>
+        {review.length > LETTER_COUNT ? (
+          <>
+            {review.slice(0, LETTER_COUNT)}
+            <Link to='' className='text-sm text-rose-500'>
+              {' '}
+              ...more
+            </Link>
+          </>
+        ) : (
+          review
+        )}
+      </p>
 
-      <div className='flex items-center gap-4'>
-        <time className='ml-auto'>
+      <div className='flex items-center gap-5 text-stone-600'>
+        <div className='space-x-1'>
+          <FaRegThumbsUp className='inline-block' />
+          <span className='text-sm font-medium'>427</span>
+        </div>
+
+        <div className='space-x-1'>
+          <FaRegThumbsDown className='inline-block' />
+          <span className='font-medium'>0</span>
+        </div>
+
+        <time className='ml-auto font-medium text-stone-400'>
           {new Date(createdAt)
             .toLocaleDateString('en-IN', {
               year: '2-digit',
